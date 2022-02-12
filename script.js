7804af00d92d30da86025fe75e1ecef8d8b72c66
CsdSreekanta <sreekanta.csd@gmail.com>
2022-02-13 00:03:17 +0600

    deposit and withdraw updated

---
+++ script.js
@@ -0,0 +1,32 @@
+//deposit
+let depositValue = document.getElementById("deposit-input");
+let depositOutput = document.getElementById('deposit-total');
+//withdraw
+let withdraValue = document.getElementById('withdraw-input');
+let withdrawOutput = document.getElementById('total-withdraw');
+//total balance
+let balanceOutput = document.getElementById('total-balance');
+
+function addMoney(currentAmount, newAmount){
+    const result =Number(currentAmount) + Number(newAmount);
+    return result;
+}
+function getMoney( currentAmount, newAmount){
+    const result = currentAmount - newAmount;
+    return result;
+}
+function deposit(){
+    
+    const totalBalance= addMoney(depositValue.value, depositOutput.innerText);
+    depositOutput.innerText = totalBalance;
+    balanceOutput.innerText= totalBalance;
+    depositValue.value ='';   
+}
+function withdraw(){
+    const totalWithdraw = addMoney(withdrawOutput.innerText, withdraValue.value);
+    withdrawOutput.innerText = totalWithdraw;
+
+const totalBalance = getMoney(balanceOutput.innerText, withdraValue.value);
+balanceOutput.innerText= totalBalance;
+   withdraValue.value ="";
+}
